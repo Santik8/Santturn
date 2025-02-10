a96f54ab29d2d23a941373d3ce25d541b4298c06
Santik8 <Santiagomaravillaramos.k@gmail.com>
2025-02-09 23:44:19 -0800

    Eliminar archivos del repositorio

--- script.js/Untitled-1.js
+++
@@ -1,9 +0,0 @@
-// Función para manejar el formulario de contacto
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-
-    alert(`Gracias por tu mensaje, ${name}. Te contactaremos en ${email}.`);
-});
